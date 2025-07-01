09eba57d00e28a8af48b5007359cf827c09cf7b8
Brandon B. Jozsa <bjozsa@jinkit.com>
2025-07-01 18:40:12 -0400

    project initial commit
    Signed-off-by: Brandon B. Jozsa <bjozsa@jinkit.com>

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proxmox-redfish",
+    version="0.1.0",
+    description="Redfish API daemon for Proxmox VMs",
+    author="Brandon B. Jozsa",
+    author_email="bjozsa@redhat.com",
+    url="https://github.com/v1k0d3n/proxmox-redfish",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        "requests_toolbelt",
+        "proxmoxer",
+    ],
+    entry_points={
+        "console_scripts": [
+            "proxmox-redfish=proxmox_redfish.proxmox_redfish:main"
+        ]
+    },
+    include_package_data=True,
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+) 
